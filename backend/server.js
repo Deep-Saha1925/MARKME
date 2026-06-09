2462a3eda87f8d7aa32590448603608c436d97f7
Deep-Saha1925 <dip23447@gmail.com>
2026-06-09 23:37:08 +0530

    Updated route for entry

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/pages/login.html'));
+});
 app.use('/api/auth',       require('./routes/auth.js'));
 app.use('/api/sessions',   require('./routes/sessions.js'));
 app.use('/api/attendance', require('./routes/attendance.js'));
